63ce675e3f75b97a1eaf2d8ba19fd0f3bad156fc
importsonu <60189040+importsonu@users.noreply.github.com>
2020-05-25 19:25:57 +0530

    Update PlayState.lua

--- match-3/src/states/PlayState.lua
+++ match-3/src/states/PlayState.lua
@@ -1,8 +1,8 @@
 --[[
-    GD50
+    GD50 2018
     Match-3 Remake
 
-    -- PlayState Class --
+    -- BeginGameState Class --
 
     Author: Colton Ogden
     cogden@cs50.harvard.edu
@@ -19,7 +19,6 @@
 PlayState = Class{__includes = BaseState}
 
 function PlayState:init()
-    
     -- start our transition alpha at full, so we fade in
     self.transitionAlpha = 255
 
@@ -39,7 +38,7 @@ function PlayState:init()
     self.score = 0
     self.timer = 60
 
-    -- set our Timer class to turn cursor highlight on and off
+    -- set our Timer class to turn highlight on cursor on and off
     Timer.every(0.5, function()
         self.rectHighlighted = not self.rectHighlighted
     end)
@@ -47,24 +46,18 @@ function PlayState:init()
     -- subtract 1 from timer every second
     Timer.every(1, function()
         self.timer = self.timer - 1
-
-        -- play warning sound on timer if we get low
-        if self.timer <= 5 then
-            gSounds['clock']:play()
-        end
     end)
 end
 
-function PlayState:enter(params)
-    
-    -- grab level # from the params we're passed
-    self.level = params.level
+function PlayState:enter(def)
+    -- grab level # from the def we're passed
+    self.level = def.level
 
     -- spawn a board and place it toward the right
-    self.board = params.board or Board(VIRTUAL_WIDTH - 272, 16)
+    self.board = def.board or Board(VIRTUAL_WIDTH - 272, 16)
 
-    -- grab score from params if it was passed
-    self.score = params.score or 0
+    -- grab score from def if it was passed
+    self.score = def.score or 0
 
     -- score we have to reach to get to the next level
     self.scoreGoal = self.level * 1.25 * 1000
@@ -77,28 +70,11 @@ function PlayState:update(dt)
 
     -- go back to start if time runs out
     if self.timer <= 0 then
-        
-        -- clear timers from prior PlayStates
-        Timer.clear()
-        
-        gSounds['game-over']:play()
-
-        gStateMachine:change('game-over', {
-            score = self.score
-        })
+        gStateMachine:change('start')
     end
 
     -- go to next level if we surpass score goal
     if self.score >= self.scoreGoal then
-        
-        -- clear timers from prior PlayStates
-        -- always clear before you change state, else next state's timers
-        -- will also clear!
-        Timer.clear()
-
-        gSounds['next-level']:play()
-
-        -- change to begin game state with new level (incremented)
         gStateMachine:change('begin-game', {
             level = self.level + 1,
             score = self.score
@@ -123,31 +99,20 @@ function PlayState:update(dt)
 
         -- if we've pressed enter, to select or deselect a tile...
         if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-            
             -- if same tile as currently highlighted, deselect
-            local x = self.boardHighlightX + 1
-            local y = self.boardHighlightY + 1
+            local x = self.boardHighlightX
+            local y = self.boardHighlightY
             
-            -- if nothing is highlighted, highlight current tile
             if not self.highlightedTile then
-                self.highlightedTile = self.board.tiles[y][x]
-
-            -- if we select the position already highlighted, remove highlight
-            elseif self.highlightedTile == self.board.tiles[y][x] then
-                self.highlightedTile = nil
-
-            -- if the difference between X and Y combined of this highlighted tile
-            -- vs the previous is not equal to 1, also remove highlight
-            elseif math.abs(self.highlightedTile.gridX - x) + math.abs(self.highlightedTile.gridY - y) > 1 then
-                gSounds['error']:play()
+                self.highlightedTile = self.board.tiles[(y * 8 + x) + 1]
+            elseif self.highlightedTile == self.board.tiles[(y * 8 + x) + 1] then
                 self.highlightedTile = nil
             else
-                
                 -- swap grid positions of tiles
                 local tempX = self.highlightedTile.gridX
                 local tempY = self.highlightedTile.gridY
 
-                local newTile = self.board.tiles[y][x]
+                local newTile = self.board.tiles[(y * 8 + x) + 1]
 
                 self.highlightedTile.gridX = newTile.gridX
                 self.highlightedTile.gridY = newTile.gridY
@@ -155,20 +120,50 @@ function PlayState:update(dt)
                 newTile.gridY = tempY
 
                 -- swap tiles in the tiles table
-                self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] =
+                self.board.tiles[(self.highlightedTile.gridY - 1) * 8 + self.highlightedTile.gridX] =
                     self.highlightedTile
 
-                self.board.tiles[newTile.gridY][newTile.gridX] = newTile
+                self.board.tiles[(newTile.gridY - 1) * 8 + newTile.gridX] = newTile
 
                 -- tween coordinates between the two so they swap
                 Timer.tween(0.1, {
                     [self.highlightedTile] = {x = newTile.x, y = newTile.y},
                     [newTile] = {x = self.highlightedTile.x, y = self.highlightedTile.y}
                 })
-                
                 -- once the swap is finished, we can tween falling blocks as needed
                 :finish(function()
-                    self:calculateMatches()
+                    self.highlightedTile = nil
+
+                    -- if we have any matches, remove them and tween the falling blocks that result
+                    local matches = self.board:calculateMatches()
+                    
+                    if matches then
+                        
+                        -- add score for each match
+                        for k, match in pairs(matches) do
+                            self.score = self.score + #match * 50
+                        end
+
+                        -- remove any tiles that matched from the board, making empty spaces
+                        self.board:removeMatches()
+
+                        -- gets a table with tween values for tiles that should now fall
+                        local tilesToFall = self.board:getFallingTiles()
+
+                        -- first, tween the falling tiles over 0.25s
+                        Timer.tween(0.25, tilesToFall):finish(function()
+                            local newTiles = self.board:getNewTiles()
+                            
+                            -- then, tween new tiles that spawn from the ceiling over 0.25s to fill in
+                            -- the new upper gaps that exist
+                            Timer.tween(0.25, newTiles):finish(function()
+                                self.canInput = true
+                            end)
+                        end)
+                    -- if no matches, we can continue playing
+                    else
+                        self.canInput = true
+                    end
                 end)
             end
         end
@@ -177,55 +172,12 @@ function PlayState:update(dt)
     Timer.update(dt)
 end
 
---[[
-    Calculates whether any matches were found on the board and tweens the needed
-    tiles to their new destinations if so. Also removes tiles from the board that
-    have matched and replaces them with new randomized tiles, deferring most of this
-    to the Board class.
-]]
-function PlayState:calculateMatches()
-    self.highlightedTile = nil
-
-    -- if we have any matches, remove them and tween the falling blocks that result
-    local matches = self.board:calculateMatches()
-    
-    if matches then
-        gSounds['match']:stop()
-        gSounds['match']:play()
-
-        -- add score for each match
-        for k, match in pairs(matches) do
-            self.score = self.score + #match * 50
-        end
-
-        -- remove any tiles that matched from the board, making empty spaces
-        self.board:removeMatches()
-
-        -- gets a table with tween values for tiles that should now fall
-        local tilesToFall = self.board:getFallingTiles()
-
-        -- tween new tiles that spawn from the ceiling over 0.25s to fill in
-        -- the new upper gaps that exist
-        Timer.tween(0.25, tilesToFall):finish(function()
-            
-            -- recursively call function in case new matches have been created
-            -- as a result of falling blocks once new blocks have finished falling
-            self:calculateMatches()
-        end)
-    
-    -- if no matches, we can continue playing
-    else
-        self.canInput = true
-    end
-end
-
 function PlayState:render()
     -- render board of tiles
     self.board:render()
 
     -- render highlighted tile if it exists
     if self.highlightedTile then
-        
         -- multiply so drawing white rect makes it brighter
         love.graphics.setBlendMode('add')
 
@@ -245,18 +197,14 @@ function PlayState:render()
     end
 
     -- draw actual cursor rect
-    love.graphics.setLineWidth(4)
     love.graphics.rectangle('line', self.boardHighlightX * 32 + (VIRTUAL_WIDTH - 272),
         self.boardHighlightY * 32 + 16, 32, 32, 4)
 
     -- GUI text
-    love.graphics.setColor(56, 56, 56, 234)
-    love.graphics.rectangle('fill', 16, 16, 186, 116, 4)
-
     love.graphics.setColor(99, 155, 255, 255)
     love.graphics.setFont(gFonts['medium'])
-    love.graphics.printf('Level: ' .. tostring(self.level), 20, 24, 182, 'center')
-    love.graphics.printf('Score: ' .. tostring(self.score), 20, 52, 182, 'center')
-    love.graphics.printf('Goal : ' .. tostring(self.scoreGoal), 20, 80, 182, 'center')
-    love.graphics.printf('Timer: ' .. tostring(self.timer), 20, 108, 182, 'center')
+    love.graphics.print('Level: ' .. tostring(self.level), 24, 24)
+    love.graphics.print('Score: ' .. tostring(self.score), 24, 48)
+    love.graphics.print('Goal : ' .. tostring(self.scoreGoal), 24, 72)
+    love.graphics.print('Timer: ' .. tostring(self.timer), 24, 96)
 end
